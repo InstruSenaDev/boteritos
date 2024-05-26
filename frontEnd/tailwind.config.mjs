f66778cef968426744696b06c9d064fee66f4d0c
JuanSatizabal2006 <juansatizabal2701@gmail.com>
2024-05-25 21:47:20 -0500

    Creación de diseño de login
    Se realizó el diseño del login, en la configuración del Tailwind ya se implementaron los colores a utilizar en todo el aplicativo, a su vez el tamaño de fuentes para su objetivo en concreto.

--- frontEnd/tailwind.config.mjs
+++ frontEnd/tailwind.config.mjs
@@ -2,7 +2,30 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				transparent: 'transparent',
+				'darkBlue' : '#206AC4',
+				'ligthBlue' : '#5EAFE8',
+				'orange' : '#FF6D00',
+				'black': '#1E1E1E',
+				'backgroundWhite' : '#F0F4F9',
+				'white' : '#FFFFFF',
+				'placeholderBlue' : '#D6DEFA'
+			},
+			fontSize:{
+				'title' : '28px',
+				'title2' : '22px',
+				'subTitle' : '20px',
+				'subTitle2' : '18px',
+				'paragraph' :'16px',
+				'paragraph2': '14px',
+				'paragraph3' : '12px',
+				'paragraphSmall' : '8px',
+				'button' : '18px',
+				'button2' : '15px'
+			}
+		},
 	},
 	plugins: [],
 }
