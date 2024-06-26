959fc4ca81ad976b1eba7ee996cfa5865ea75456
JuanSatizabal2006 <129564629+JuanSatizabal2006@users.noreply.github.com>
2024-06-26 12:02:22 -0500

    aaaaaa

--- frontEnd/src/components/tables/tableStudents.jsx
+++ frontEnd/src/components/tables/tableStudents.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'preact/hooks'
 
-export const tableStudents = ()=>{
+export default function TableStudents(texto){
     const [infoStudent, setInfoStudent] = useState();
 
+    return (
+        <>
+            <div class="w-11 bg-darkBlue">
+                <p>{texto}</p>
+            </div>
+        </>
+    )
 }
