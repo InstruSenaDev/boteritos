12c76b6d79d10c3845eb44f850ad5e45c5451a97
JuanSatizabal2006 <juansatizabal2701@gmail.com>
2024-07-05 15:21:05 -0500

    te maldigo preact

--- frontEnd/src/components/graphics/GraphicPie.jsx
+++ frontEnd/src/components/graphics/GraphicPie.jsx
@@ -1,9 +1,22 @@
+import React from "preact/compat";
+
+import { VictoryPie } from "victory";
+
+const dataLogros = [{ x: "LP", y: "4" }, { x: "LA", y: "5" }, { x: "LN", y: "2" }];
 
 const GraphicPie = () => {
   return (
-        <>
-        </>
-  )
+    <>
+      <VictoryPie
+        data={[
+          { x: "Cats", y: 35 },
+          { x: "Dogs", y: 40 },
+          { x: "Birds", y: 55 }
+        ]}
+      />
+
+    </>
+  );
 }
 
-export default GraphicPie
+export default GraphicPie;
