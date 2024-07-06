bee72a5c1dea0ac2b3d15e32fb850e74307aa884
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-07-06 15:07:36 -0500

    guardo para hacer merge

---
+++ frontEnd/src/helper/objects/studentsTeacher.js
@@ -0,0 +1,20 @@
+export const objStudentsTeacher = [
+  {
+    "name":"Sebastian Rodriguez Prado",
+    "diagnostic":"TDHA",
+    "qualify":true,
+    "area":"Teatro"
+  },
+  {
+    "name":"Juan Jose Cuartas",
+    "diagnostic":"Dawn",
+    "qualify":false,
+    "area":"Teatro"
+  },
+  {
+    "name":"Brian David Marin",
+    "diagnostic":"Imperactividad",
+    "qualify":false,
+    "area":"Teatro"
+  }
+]
