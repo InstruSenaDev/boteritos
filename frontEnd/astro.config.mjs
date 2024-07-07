a9b60a75d3c33657403e80a5d5ffb3687399b84d
BrianDmH <bryandmh564@gmail.com>
2024-07-07 18:20:41 -0500

    sidebar casi terminada
    Se arreglaron detalles como la posición y el comportamiento de la sidebar al expandirse, faltan pequeños detalles

--- frontEnd/astro.config.mjs
+++ frontEnd/astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import starlight from "@astrojs/starlight";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), starlight(), 
+  integrations: [tailwind(), react() 
   ],
   output: 'server' 
 });
