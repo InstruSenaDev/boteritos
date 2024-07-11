6ac023687887d599e88a407c922f478a2a8af1dd
JuanSatizabal2006 <juansatizabal2701@gmail.com>
2024-07-11 11:48:19 -0500

    Porqué se me borró todo :c

--- backEnd/backEndAPI/settings.py
+++ backEnd/backEndAPI/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'backEndAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'boteritos',
+        'USER' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
