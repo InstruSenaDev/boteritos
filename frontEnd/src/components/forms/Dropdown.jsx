e8b1d7d709e168b7d593ff38ba820b675ff2f60d
BrianDmH <bryandmh564@gmail.com>
2024-07-11 18:17:30 -0500

    en proceso
    .

--- frontEnd/src/components/forms/Dropdown.jsx
+++ frontEnd/src/components/forms/Dropdown.jsx
@@ -1,13 +1,20 @@
-import { Dropdown, DropdownDivider, DropdownItem } from "flowbite-react";
 
-export function DropdownB() {
+import { Select, SelectItem } from '@tremor/react';
+
+export function SelectHero() {
   return (
-    <Dropdown label="Dropdown button" dismissOnClick={false}>
-      <DropdownItem>Dashboard</DropdownItem>
-      <DropdownItem>Settings</DropdownItem>
-      <DropdownItem>Earnings</DropdownItem>
-      <DropdownItem>Sign out</DropdownItem>
-    </Dropdown>
+    <div className="max-w-[400px] flex flex-col gap-2 w-full">
+      <div className="text-paragraph font-cocogooseLight text-black">
+        Select
+      </div>
+      <Select
+        defaultValue="1"
+        className=" rounded-xl w-full px-[1px] pb-[5px] text-paragraph2 border-darkBlue font-cocogooseLight border-[1.5px]"
+      >
+        <SelectItem value="1">Option One</SelectItem>
+        <SelectItem value="2">Option Two</SelectItem>
+        <SelectItem value="3">Option Three</SelectItem>
+      </Select>
+    </div>
   );
 }
-
