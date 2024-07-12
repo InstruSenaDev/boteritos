1b39988f4b4d7b82f40c77d967f306c75a9b0487
BrianDmH <bryandmh564@gmail.com>
2024-07-12 18:37:34 -0500

    Cocinando formulario de registro
    prueba de validación y envío de datos

--- frontEnd/src/components/forms/Dropdown.jsx
+++ frontEnd/src/components/forms/Dropdown.jsx
@@ -2,16 +2,18 @@
 import { Select, SelectItem } from '@tremor/react';
 import { dataDropdown } from '../../helper/objects/dropdownArray';
 
-export function Dropdown({name}) {
+export function Dropdown({label, name}) {
   return (
     <div className="max-w-[400px] flex flex-col gap-2 w-full">
       <div className="text-paragraph font-cocogooseLight text-black">
-        {name}
+        {label}
       </div>
       <div className='text-paragraph3 font-cocogooseLight  h-10' >
         <Select
           defaultValue="4"
           className=" w-full border-[1.5px] border-darkBlue rounded-xl focus:bg-darkBlue focus:text-white" 
+          name={name}
+          required
         >
           <SelectItem value= "4">Selecciona una opción</SelectItem>
           {dataDropdown.map((dropdownKey)=>(
