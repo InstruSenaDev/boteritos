460bcb9dee4a38d0f8f236145a9b43c833dbe00f
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-07-15 17:07:48 -0500

    Intento de tabla responsive
    Falta agregar el evento para cuando presione click :cry:

--- frontEnd/src/components/search/Buscador.jsx
+++ frontEnd/src/components/search/Buscador.jsx
@@ -2,12 +2,12 @@
 export default function Buscador() {
   return (
     <form class="form relative">
-      <div class="relative">
-        <button class="absolute inset-y-[18px] left-2 flex items-center p-1">
+      <div class="relative flex  justify-center sm:justify-start">
+        <button class="absolute inset-y-[18px] left-5 sm:left-2 flex items-center p-1">
           <i class="fas fa-search text-[#9ca3af] w-5 h-5"></i>
         </button>
         <input
-          class="w-80 max-w-80 h-10 input rounded-2xl pl-10 pr-3 py-4 border-2 border-darkBlue focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
+          class="sm:w-80 sm:max-w-80 h-10 input rounded-2xl pl-10 pr-3 py-4 border-2 border-darkBlue focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
           placeholder="Buscar..."
           required=""
           type="text"
--- frontEnd/src/components/tables/tableListaLogros.jsx
+++ frontEnd/src/components/tables/tableListaLogros.jsx
@@ -1,45 +1,73 @@
 import DataState from "./dataStates/DataState";
 import { ObjLogrosCreados } from "../../helper/objects/ListaLogros";
 import Buscador from "../search/Buscador";
+import "../../components/tables/input.css";
+import { useState } from "react";
 
-export default function TableListaLogros(){
-  return(
+export default function TableListaLogros() {
+  const[openAcc, setOpenAcc] =useState(false);
+  return (
     <>
       <main className="bg-white rounded-xl py-7 px-8 w-full overflow-y-hidden">
-        
         {/*Buscador*/}
-       <Buscador/>
-      
+        <Buscador />
+
         <section className="max-h-[80vh] overflow-y-scroll">
           {/*HEADER TABLA*/}
-          <div className="sticky top-0 bg-white sm:flex sm:justify-between lg:grid grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] gap-x-8 text-paragraph font-cocogooseLight text-darkBlue p-5 border-b-2 border-b-placeholderBlue hidden">
+          <div className="sticky hidden top-0 bg-white lg:grid grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] gap-x-8 text-paragraph font-cocogooseLight text-darkBlue p-5 border-b-2 border-b-placeholderBlue">
             <p>No°</p>
             <p>Nombre del logro</p>
             <p>Fecha</p>
             <p>Estado</p>
-            <p>Tipo</p>
-            
-
+            <p className="sm:justify-self-center">Tipo</p>
           </div>
 
           {/*CUERPO DE LA TABLA */}
-          {ObjLogrosCreados.map((data,index)=>(
-            <div className="grid grid-cols-1 sm:flex sm:justify-between lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue">
-                
-              <p>{(index + 1).toString().length == 2 ? index + 1 : `0${index + 1}`}</p>
-              <p>{`${data.achievement}`}</p>
-              <p>{`${data.date}`}</p>
-              <DataState state={data.state} />
-              <p>{`${data.type}`}</p>
+          {ObjLogrosCreados.map((data, index) => (
+            <div className="acc-item close grid grid-cols-1 lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-x-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue">
+              <div className="flex gap-2 lg:gap-0 ">
+                <p className="text-darkBlue lg:hidden">No°</p>
+                <div className="acc-header w-full">
+                  <p>
+                    {(index + 1).toString().length == 2
+                      ? index + 1
+                      : `0${index + 1}`}
+                  </p>
+                  <button><i class="fa-solid fa-angle-down block lg:hidden"></i></button>
+                </div>
+              </div>
 
-        
+              <div className="flex gap-2 lg:gap-0">
+                <p className="text-darkBlue lg:hidden">Logro:</p>
+                <div className="acc-header w-full">
+                  <p>{`${data.achievement}`}</p>{" "}
+                </div>
+              </div>
+
+            <div className="flex gap-2 lg:gap-0 acc-body">
+              <p className="text-darkBlue lg:hidden">Fecha:</p>
+                <div className="">
+                  <p>{`${data.date}`}</p>
+                </div>
+              </div>
+
+              <div className="flex gap-2 lg:gap-0 acc-body ">
+              <p className="text-darkBlue lg:hidden">Estado:</p>
+                <div className="">
+                  <DataState state={data.state} />
+                </div>
+              </div>
+
+              <div className="flex gap-2 lg:gap-0 acc-body">
+              <p className="text-darkBlue lg:hidden">Tipo:</p>
+                <div className=" sm:justify-self-center">
+                  <p>{`${data.type}`}</p>
+                </div>
+              </div>
             </div>
           ))}
-
         </section>
-
-
       </main>
     </>
-  )
+  );
 }
