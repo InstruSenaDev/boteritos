8b07f567659a73cf2ee39518cc324b9c42db0004
Aleej0 <161045194+BrianDmH@users.noreply.github.com>
2024-07-17 10:37:30 -0500

    .
    hola

---
+++ frontEnd/src/components/forms/Input.jsx
@@ -0,0 +1,14 @@
+export const Input = ({texto, placeholder, name, tipo}) => {
+    return(
+        <>
+        <div class="max-w-[400px] flex flex-col gap-2 text-xd w-full">
+            <label for="input" class="text-paragraph font-cocogooseLight text-black">{texto}</label>
+            <div class="relative w-full flex">
+                <input required type={tipo} id="input" name={name} placeholder={placeholder} class="h-10 rounded-xl w-full px-5 pb-[5px] text-paragraph3 border-darkBlue font-cocogooseLight border-[1.5px]" />
+                <img src="../../../public/img/icons.svg" alt=""/>
+            </div>
+
+        </div>
+        </>
+    )
+    }
