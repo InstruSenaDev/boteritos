8f0ef048d8cec4432a59a2b37e9f32957a5a51d3
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-07-18 08:03:27 -0500

    Arreglo de el acordion en dispositivos moviles y borrado del input.css

--- frontEnd/src/components/tables/tableListaLogros.jsx
+++ frontEnd/src/components/tables/tableListaLogros.jsx
@@ -1,11 +1,17 @@
 import DataState from "./dataStates/DataState";
 import { ObjLogrosCreados } from "../../helper/objects/ListaLogros";
 import Buscador from "../search/Buscador";
-import "../../components/tables/input.css";
+import "../../../input.css";
 import { useState } from "react";
 
 export default function TableListaLogros() {
-  const[openAcc, setOpenAcc] =useState(false);
+  const [openAcc, setOpenAcc] = useState(-1);
+
+  const toogleRow = (index) =>{
+    openAcc != index ? setOpenAcc(index) : setOpenAcc(-1)
+  }
+
+  console.log(openAcc);
   return (
     <>
       <main className="bg-white rounded-xl py-7 px-8 w-full overflow-y-hidden">
@@ -24,16 +30,16 @@ export default function TableListaLogros() {
 
           {/*CUERPO DE LA TABLA */}
           {ObjLogrosCreados.map((data, index) => (
-            <div className="acc-item close grid grid-cols-1 lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-x-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue">
+            <div className={`acc-item grid grid-cols-1 lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-x-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue ${openAcc == index ? "open" : "close"}`} key={index}>
               <div className="flex gap-2 lg:gap-0 ">
                 <p className="text-darkBlue lg:hidden">No°</p>
-                <div className="acc-header w-full">
+                <div className="acc-header w-full flex justify-between items-center ">
                   <p>
                     {(index + 1).toString().length == 2
                       ? index + 1
                       : `0${index + 1}`}
                   </p>
-                  <button><i class="fa-solid fa-angle-down block lg:hidden"></i></button>
+                  <button onClick={() => toogleRow(index) }><i className="fa-solid fa-angle-down block lg:hidden"></i></button>
                 </div>
               </div>
 
@@ -44,23 +50,23 @@ export default function TableListaLogros() {
                 </div>
               </div>
 
-            <div className="flex gap-2 lg:gap-0 acc-body">
-              <p className="text-darkBlue lg:hidden">Fecha:</p>
+              <div className="flex gap-2 lg:gap-0 acc-body">
+                <p className="text-darkBlue lg:hidden">Fecha:</p>
                 <div className="">
                   <p>{`${data.date}`}</p>
                 </div>
               </div>
 
               <div className="flex gap-2 lg:gap-0 acc-body ">
-              <p className="text-darkBlue lg:hidden">Estado:</p>
+                <p className="text-darkBlue lg:hidden">Estado:</p>
                 <div className="">
                   <DataState state={data.state} />
                 </div>
               </div>
 
-              <div className="flex gap-2 lg:gap-0 acc-body">
-              <p className="text-darkBlue lg:hidden">Tipo:</p>
-                <div className=" sm:justify-self-center">
+              <div className="flex gap-2 lg:gap-0 acc-body sm:justify-self-center">
+                <p className="text-darkBlue lg:hidden">Tipo:</p>
+                <div >
                   <p>{`${data.type}`}</p>
                 </div>
               </div>
