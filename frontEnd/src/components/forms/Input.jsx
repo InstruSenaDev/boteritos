66cc3bcae8f0f9cf09e3c5912d571686f5bca31a
BrianDmH <bryandmh564@gmail.com>
2024-07-19 16:46:48 -0500

    registro "Funcional"
    Falta css al upload input

--- frontEnd/src/components/forms/Input.jsx
+++ frontEnd/src/components/forms/Input.jsx
@@ -1,14 +1,20 @@
-export const Input = ({texto, placeholder, name, tipo}) => {
-    return(
-        <>
+export const Input = ({ texto, placeholder, name, tipo, onChange, value }) => {
+    return (
         <div class="max-w-[400px] flex flex-col gap-2 text-xd w-full">
-            <label for="input" class="text-paragraph font-cocogooseLight text-black">{texto}</label>
+            <label htmlFor={name} class="text-paragraph font-cocogooseLight text-black">{texto}</label>
             <div class="relative w-full flex">
-                <input required type={tipo} id="input" name={name} placeholder={placeholder} class="h-10 rounded-xl w-full px-5 text-paragraph3 border-darkBlue font-cocogooseLight border-[1.5px]" />
-                {/*<img src="../../../public/img/icons.svg" alt=""/>*/}
+                <input
+                    required
+                    type={tipo}
+                    id={name}
+                    name={name}
+                    placeholder={placeholder}
+                    class="h-10 rounded-xl w-full px-5 text-paragraph3 border-darkBlue font-cocogooseLight border-[1.5px]"
+                    onChange={onChange}
+                    value={value}
+                />
+                {/* <img src="../../../public/img/icons.svg" alt=""/> */}
             </div>
-
         </div>
-        </>
-    )
-    }
+    );
+};
