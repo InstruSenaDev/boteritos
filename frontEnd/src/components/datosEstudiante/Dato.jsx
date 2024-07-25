91907d87ef5abff130969d4b8ad72e2641b76f3e
BrianDmH <bryandmh564@gmail.com>
2024-07-24 22:13:02 -0500

    .
    .

--- frontEnd/src/components/datosEstudiante/Dato.jsx
+++ frontEnd/src/components/datosEstudiante/Dato.jsx
@@ -1,8 +1,10 @@
-export const Dato = (tituloDato, dato) => {
-    return (
-        <div class="flex flex-col sm:flex-row gap-x-3">
-            <p class="font-cocogooseLight text-paragraph text-darkBlue">{tituloDato}:</p>
-            <p class="font-cocogooseLight text-paragraph2 flex-1">{dato}</p>
-        </div>
-    )
-}
+import React from 'react';
+
+export const Dato = ({ tituloDato, dato }) => {
+  return (
+    <div className="flex flex-col sm:flex-row gap-x-3">
+      <p className="font-cocogooseLight text-paragraph text-darkBlue">{tituloDato}:</p>
+      <p className="font-cocogooseLight text-paragraph2 flex-1">{dato}</p>
+    </div>
+  );
+};
--- frontEnd/src/components/datosEstudiante/GrupoDatos.jsx
+++ frontEnd/src/components/datosEstudiante/GrupoDatos.jsx
@@ -1,24 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export const GrupoDatos = (titulo, children) => {
-    return (
-        <div className="w-full bg-white p-6 rounded-xl flex flex-col gap-y-5">
-          <div className="flex justify-between text-darkBlue">
-            <p className="font-cocogooseSemiLight text-subTitle">{titulo}</p>
-            <p className="font-cocogooseSemiLight text-subTitle2 underline">Editar</p>
-          </div>
-          <div className="flex flex-col xl:flex-row px-5 justify-between gap-y-3">
-            <div className="flex flex-col w-full gap-y-3">
-              {React.Children.map(children, child => (
-                child.props.slot === 'columnaD' && child
-              ))}
-            </div>
-            <div className="flex flex-col w-full gap-y-3">
-              {React.Children.map(children, child => (
-                child.props.slot === 'columnaI' && child
-              ))}
-            </div>
-          </div>
+export const GrupoDatos = ({ titulo, children }) => {
+  return (
+    <div className="w-full bg-white p-6 rounded-xl flex flex-col gap-y-5">
+      <div className="flex justify-between text-darkBlue">
+        <p className="font-cocogooseSemiLight text-subTitle">{titulo}</p>
+        <p className="font-cocogooseSemiLight text-subTitle2 underline">Editar</p>
+      </div>
+      <div className="flex flex-col xl:flex-row px-5 justify-between gap-y-3">
+        <div className="flex flex-col w-full gap-y-3">
+          {children.columnaD}
         </div>
-      );
-    };
+        <div className="flex flex-col w-full gap-y-3">
+          {children.columnaI}
+        </div>
+      </div>
+    </div>
+  );
+};
