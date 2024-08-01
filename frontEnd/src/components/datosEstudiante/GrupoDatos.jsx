fe08d527c1af03e9a98620e59a6811f126b8802b
BrianDmH <bryandmh564@gmail.com>
2024-08-01 18:51:55 -0500

    Vista datos de estudiante full optimizada
    Se pasaron componentes de astro a react utilizando children, se mapeó y modularizó la vista de datos de estudiante por parte de admin.

--- frontEnd/src/components/datosEstudiante/GrupoDatos.jsx
+++ frontEnd/src/components/datosEstudiante/GrupoDatos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const GrupoDatos = ({ titulo, children }) => {
+export const GrupoDatos = ({ titulo, children}) => {
   return (
     <div className="w-full bg-white p-6 rounded-xl flex flex-col gap-y-5">
       <div className="flex justify-between text-darkBlue">
@@ -8,11 +8,8 @@ export const GrupoDatos = ({ titulo, children }) => {
         <p className="font-cocogooseSemiLight text-subTitle2 underline">Editar</p>
       </div>
       <div className="flex flex-col xl:flex-row px-5 justify-between gap-y-3">
-        <div className="flex flex-col w-full gap-y-3">
-          {children.columnaD}
-        </div>
-        <div className="flex flex-col w-full gap-y-3">
-          {children.columnaI}
+        <div className="grid grid-cols-2 w-full gap-y-3">
+          {children}
         </div>
       </div>
     </div>
