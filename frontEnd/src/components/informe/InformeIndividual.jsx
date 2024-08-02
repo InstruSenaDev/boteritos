2a16c931e91b7584c8bcc8c43e7ab9a70f0a8a08
Brian <161045194+BrianDmH@users.noreply.github.com>
2024-08-02 11:44:54 -0500

    .
    .

--- frontEnd/src/components/informe/InformeIndividual.jsx
+++ frontEnd/src/components/informe/InformeIndividual.jsx
@@ -1,4 +1,4 @@
-export const InformeIndividual = ({tituloArea}) => {
+export const InformeIndividual = ({tituloArea, logroTexto, data}) => {
     return(
         <div className="w-full bg-white rounded-xl flex flex-col px-10 py-5">
             <div className="flex justify-between text-darkBlue">
@@ -7,6 +7,14 @@ export const InformeIndividual = ({tituloArea}) => {
                     <p>L.A</p>
                     <p>L.P</p>
                     <p>L.N</p>
+                </div>
+            </div>
+            <div className="h-[66px] py-5  flex justify-between">
+                <p className="font-cocogooseLight text-paragraph">{logroTexto}</p>
+                <div className="flex font-cocogooseSemiLight text-title2 text-orange max-w-[250px] w-full justify-between px-[10px]">
+                    <p>X</p>
+                    <p>X</p>
+                    <p>X</p>
                 </div>
             </div>
         </div>
