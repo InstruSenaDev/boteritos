ecbe4fed2eaf6dec01b839d137b36169c95f439a
D4vz4r <161045194+BrianDmH@users.noreply.github.com>
2024-08-02 09:07:39 -0500

    .
    .

--- frontEnd/src/components/informe/InformeIndividual.jsx
+++ frontEnd/src/components/informe/InformeIndividual.jsx
@@ -1,8 +1,13 @@
-export const InformeIndividual = () => {
+export const InformeIndividual = ({tituloArea}) => {
     return(
         <div className="w-full bg-white rounded-xl flex flex-col px-10 py-5">
             <div>
-                
+                <p>{tituloArea}</p>
+                <div>
+                    <p>L.A</p>
+                    <p>L.P</p>
+                    <p>L.N</p>
+                </div>
             </div>
         </div>
     )
