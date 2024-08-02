e283a82c6ca616ff2c58cb3c2046cd722fd6982d
Brian <161045194+BrianDmH@users.noreply.github.com>
2024-08-02 10:38:52 -0500

    .
    .

--- frontEnd/src/components/informe/InformeIndividual.jsx
+++ frontEnd/src/components/informe/InformeIndividual.jsx
@@ -1,9 +1,9 @@
 export const InformeIndividual = ({tituloArea}) => {
     return(
         <div className="w-full bg-white rounded-xl flex flex-col px-10 py-5">
-            <div className="flex">
-                <p>{tituloArea}</p>
-                <div className="flex ">
+            <div className="flex justify-between text-darkBlue">
+                <p className="font-cocogooseSemiLight text-subTitle">{tituloArea}</p>
+                <div className="flex font-cocogooseSemiLight text-title2 max-w-[250px] w-full justify-between">
                     <p>L.A</p>
                     <p>L.P</p>
                     <p>L.N</p>
