a2a80e8848724209f9cd7977d6a4c799117d2049
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-08-01 21:20:54 -0500

    modal

--- frontEnd/src/components/modales/Modal.jsx
+++ frontEnd/src/components/modales/Modal.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Button, Dialog, DialogPanel } from "@tremor/react";
-import { Input } from "../forms/Input";
-import { Dropdown } from "../forms/Dropdown";
 export function Modal(props) {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
@@ -10,42 +8,16 @@ export function Modal(props) {
         {props.txtboton}
       </Button>
       <Dialog open={isOpen} onClose={(val) => setIsOpen(val)} static={true}>
-        <DialogPanel className="flex flex-col gap-8 items-center min-w-[800px] w-full py-[30px] px-[20px]">
+        <DialogPanel className="flex flex-col gap-8 items-start min-w-[800px] w-full py-[40px] px-[30px]">
           <h1 className="text-title text-darkBlue">{props.txtmodal}</h1>
-          <div className="flex gap-x-8 w-full">
-          <div className="flex flex-col gap-8 bg-red-50 w-full items-center ">
-       
-          <Input texto="Nombre completo"
-            placeholder="Ingresa tu documento"
-            name="ndocumento"
-            tipo="text" />
-          <Input texto="Número de documento"
-            placeholder="Ingresa tu documento"
-            name="ndocumento"
-            tipo="text" /><Input texto="Número de documento"
-              placeholder="Ingresa tu documento"
-              name="ndocumento"
-              tipo="text" />
+          <div className="w-full">
+            {props.children}
           </div>
-
-          <div className="flex flex-col gap-8  bg-red-50 w-full items-center ">
-       
-          <Input texto="Otro télefono"
-            placeholder="Ingresa tu documento"
-            name="ndocumento"
-            tipo="text" />
-          <Input texto="Número de documento"
-            placeholder="Ingresa tu documento"
-            name="ndocumento"
-            tipo="text" /><Input texto="Número de documento"
-              placeholder="Ingresa tu documento"
-              name="ndocumento"
-              tipo="text" />
-          </div>
-          </div>
-          <Button className="mt-8 w-full" onClick={() => setIsOpen(false)}>
-            Cerrar
+          <div className="flex justify-center w-full">
+          <Button className="max-w-[400px] w-full" onClick={() => setIsOpen(false)}>
+            {props.txtbutton2}
           </Button>
+          </div>
         </DialogPanel>
       </Dialog>
     </>
--- frontEnd/src/components/tables/tableListaLogros.jsx
+++ frontEnd/src/components/tables/tableListaLogros.jsx
@@ -5,18 +5,16 @@ import "../../../input.css";
 import { useState } from "react";
 import React from "react";
 import { Modal } from "../modales/Modal";
-
-
+import { Input } from "../forms/Input";
+import { Dropdown } from "../forms/Dropdown";
+import { dataDoc } from "../../helper/objects/dropdownArray";
 
 export default function TableListaLogros() {
-
-
-
   const [openAcc, setOpenAcc] = useState(-1);
 
-  const toogleRow = (index) =>{
-    openAcc != index ? setOpenAcc(index) : setOpenAcc(-1)
-  }
+  const toogleRow = (index) => {
+    openAcc != index ? setOpenAcc(index) : setOpenAcc(-1);
+  };
 
   console.log(openAcc);
   return (
@@ -24,10 +22,65 @@ export default function TableListaLogros() {
       <main className="bg-white rounded-xl py-7 px-8 w-full overflow-y-hidden">
         {/*Buscador*/}
         <div className="flex justify-between w-full pb-5">
-        <Buscador />
-        <Modal txtboton="Abreme" txtmodal="Agregar datos medicos">
-          
-        </Modal>
+          <Buscador />
+          <Modal
+            txtboton="Abreme"
+            txtmodal="Agregar datos medicos"
+            txtbutton2="AGREGAR"
+          >
+            <div className="grid grid-cols-2 gap-x-[30px] gap-y-[20px]">
+              <Input
+                texto="Nombre completo"
+                placeholder="Ingresa tu documento"
+                name="nombre"
+                tipo="text"
+              />
+              <Dropdown
+                name={"documento"}
+                label={"Tipo de documento"}
+                data={dataDoc}
+                onChange={(value) => handleDropdownChange("documento", value)}
+              />
+
+              <Input
+                texto="Número de documento"
+                placeholder="Ingresa tu documento"
+                name="ndocumento"
+                tipo="text"
+              />
+              <Input
+                texto="Teléfono"
+                placeholder="Ingresa tu documento"
+                name="telefono"
+                tipo="text"
+              />
+              <Input
+                texto="Otro teléfono"
+                placeholder="Ingresa tu documento"
+                name="telefonodos"
+                tipo="text"
+              />
+
+              <Input
+                texto="Dirección"
+                placeholder="Ingresa tu documento"
+                name="direccion"
+                tipo="text"
+              />
+              <Input
+                texto="Empresa"
+                placeholder="Ingresa tu documento"
+                name="ndocumento"
+                tipo="text"
+              />
+              <Input
+                texto="Tipo parentezco"
+                placeholder="Ingresa tu documento"
+                name="ndocumento"
+                tipo="text"
+              />
+            </div>
+          </Modal>
         </div>
 
         <section className="max-h-[80vh] overflow-y-scroll">
@@ -42,7 +95,12 @@ export default function TableListaLogros() {
 
           {/*CUERPO DE LA TABLA */}
           {ObjLogrosCreados.map((data, index) => (
-            <div className={`acc-item grid grid-cols-1 lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-x-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue ${openAcc == index ? "open" : "close"}`} key={index}>
+            <div
+              className={`acc-item grid grid-cols-1 lg:grid-cols-[50px_minmax(550px,_1fr)_minmax(50px,_1fr)_minmax(150px,_1fr)_minmax(250px,1fr)] items-center gap-x-8 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue ${
+                openAcc == index ? "open" : "close"
+              }`}
+              key={index}
+            >
               <div className="flex gap-2 lg:gap-0 ">
                 <p className="text-darkBlue lg:hidden">No°</p>
                 <div className="acc-header w-full flex justify-between items-center ">
@@ -51,7 +109,9 @@ export default function TableListaLogros() {
                       ? index + 1
                       : `0${index + 1}`}
                   </p>
-                  <button onClick={() => toogleRow(index) }><i className="fa-solid fa-angle-down block lg:hidden"></i></button>
+                  <button onClick={() => toogleRow(index)}>
+                    <i className="fa-solid fa-angle-down block lg:hidden"></i>
+                  </button>
                 </div>
               </div>
 
@@ -78,7 +138,7 @@ export default function TableListaLogros() {
 
               <div className="flex gap-2 lg:gap-0 acc-body sm:justify-self-center">
                 <p className="text-darkBlue lg:hidden">Tipo:</p>
-                <div >
+                <div>
                   <p>{`${data.type}`}</p>
                 </div>
               </div>
@@ -86,7 +146,6 @@ export default function TableListaLogros() {
           ))}
         </section>
       </main>
-
     </>
   );
 }
