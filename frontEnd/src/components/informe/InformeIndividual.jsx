3865a3a000b8387195c671c357d053e3160e1fd5
D4vz4r <161045194+BrianDmH@users.noreply.github.com>
2024-08-02 08:46:49 -0500

    .
    .

---
+++ frontEnd/src/components/informe/InformeIndividual.jsx
@@ -0,0 +1,9 @@
+export const InformeIndividual = () => {
+    return(
+        <div className="w-full bg-white rounded-xl flex flex-col px-10 py-5">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
