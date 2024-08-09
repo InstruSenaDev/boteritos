83e51cfa74a0cc1729a63e525ad5e820aec80f41
D4vz4r <161045194+BrianDmH@users.noreply.github.com>
2024-08-09 08:55:37 -0500

    en proceso
    .

--- frontEnd/src/components/informe/Observacion.jsx
+++ frontEnd/src/components/informe/Observacion.jsx
@@ -9,9 +9,11 @@ export const Observacion = () => (
     <Accordion className=''>
         <AccordionHeader className="font-cocogooseSemiLight  text-darkBlue">Ver Observación</AccordionHeader>
         <AccordionBody className="font-cocogooseSemiLight text-subTitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            tempor lorem non est congue blandit. Praesent non lorem sodales,
-            suscipit est sed, hendrerit dolor.
+            <span className='font-cocogooseLight text-paragraph'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                tempor lorem non est congue blandit. Praesent non lorem sodales,
+                suscipit est sed, hendrerit dolor.
+            </span>
         </AccordionBody>
     </Accordion>
 );
