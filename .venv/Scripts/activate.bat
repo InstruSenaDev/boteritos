4777204bfffa2723c3dbc773e912776a2ba299b7
JuanSatizabal <129564629+JuanSatizabal2006@users.noreply.github.com>
2024-08-29 07:14:43 -0500

    AY NO ESTE VENV ME TIENE MAMADOOOOO
    culpa de brian

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\bryan\OneDrive\Documentos\dashboard\boteritos\.venv
+set VIRTUAL_ENV=c:\Users\jjcs2\OneDrive\Documentos\GitHub\boteritos\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
