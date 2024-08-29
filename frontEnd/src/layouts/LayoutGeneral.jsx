8e00edbba277e48ce6d008fd1b70d3be9a0f71a0
BrianDmH <bryandmh564@gmail.com>
2024-08-29 00:21:57 -0500

    la vida es una mrd
    si

--- frontEnd/src/layouts/LayoutGeneral.jsx
+++ frontEnd/src/layouts/LayoutGeneral.jsx
@@ -5,6 +5,9 @@ import React from "react";
 
 
 export const LayoutGeneral = ({titleHeader, children }) => {
+  // Obtener el rol del usuario desde el localStorage y parsearlo
+  //  const user = JSON.parse(localStorage.getItem('dataUser'));
+  //  const rol = user?.idrol;
   return (
       <Layout>
         <Sidebar name={"Brian David Marin Hernandez"} rol={"Administrador"}/>
