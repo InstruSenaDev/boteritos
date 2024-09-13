8fa9830a74187f60f437f66122ba178a8ddc3aae
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-09-13 02:17:34 -0500

    Post modificado

--- frontEnd/src/api/fetch.js
+++ frontEnd/src/api/fetch.js
@@ -20,17 +20,12 @@ export const fetchFunction = async (method, body, header , url) =>{
 
 export const fetchFunctionFiles = async (method, body, header , url) =>{
 
-    let dataBody = !body ? null : JSON.stringify(body);
-
     const response = await fetch(`${urlApi}${url}`, {
         method: method,
-        body : dataBody,
-        headers: {
-            'Content-Type': 'multipart/form-data' 
-        }
+        body : body
+        
     })
 
-    
     const data = await response.json()
     const result = {status: response.status, data : data }
     return result
