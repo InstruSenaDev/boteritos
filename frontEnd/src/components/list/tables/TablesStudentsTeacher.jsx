db00d5ede2aee2cc09f2fc2795d88ae503d4e641
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-09-17 18:11:53 -0500

    Tabla mapeada

--- frontEnd/src/components/list/tables/TablesStudentsTeacher.jsx
+++ frontEnd/src/components/list/tables/TablesStudentsTeacher.jsx
@@ -1,11 +1,13 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Buscador from "../../search/Buscador";
 import DataState from "../dataStates/DataState";
-import { objStudentsTeacher } from "../../../helper/objects/studentsTeacher";
+import { getAllUser } from "../../../api/get";
 
 
-export default function TablesStudentsTeacher() {
+export default function TablesStudentsTeacher(getId) {
+
+const [dataStudents, setDataStudents] = useState([]);
 
   const [openAcc, setOpenAcc] = useState(-1);
 
@@ -13,7 +15,14 @@ export default function TablesStudentsTeacher() {
     openAcc != index ? setOpenAcc(index) : setOpenAcc(-1);
   }
 
-
+  useEffect(() => {
+    const obtainData = async () => {
+      const dataApi = await getAllUser("sql/estudiantes/tabla");
+      console.log(dataApi);
+      setDataStudents(dataApi.data);
+    };
+    obtainData();
+  }, []);
 
   return (
     <>
@@ -35,55 +44,59 @@ export default function TablesStudentsTeacher() {
           </div>
 
           {/*CUERPO DE LA TABLA */}
-          {objStudentsTeacher.map((data, index) => (
-            <div
-            className={`acc-item grid grid-cols-1 lg:grid-cols-[150px_minmax(350px,1fr)_minmax(250px,_1fr)_repeat(2,_minmax(100px,_1fr))] items-center gap-x-3 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue ${
-              openAcc === index ? "open" : "close"
-            }`}
-            key={index}
-          >
-              <div className="flex gap-2 lg:gap-0">
-                <p className="text-darkBlue lg:hidden">No°:</p>
-                <div className="acc-header w-full flex justify-between items-center">
-                  <p>
-                    {(index + 1).toString().length == 2
-                      ? index + 1
-                      : `0${index + 1}`}
-                  </p>
-                  <button onClick={() => toogleRow(index)}><i className="fa-solid fa-angle-down block lg:hidden"></i></button>
-                </div>
-              </div>
-              <div className="flex gap-2 lg:gap-0 ">
-                <p className="text-darkBlue lg:hidden">Nombre:</p>
-                <div className=" flex justify-self-center acc-header">
-                  <p className="underline">{`${data.name} ${data.lastname}`}</p>
-                </div>
-              </div>
-
-              <div className="flex gap-2 lg:gap-0 acc-body ">
-                <p className="text-darkBlue lg:hidden">Diagnostico:</p>
-                <div className="flex justify-self-center">
-                  <p>{`${data.diagnostic}`}</p>
-                </div>
-              </div>
-
-              <div className="acc-body flex gap-2 lg:gap-0">
-                  <p className="text-darkBlue lg:hidden">Calificación:</p>
-                  <div className="w-full flex justify-between items-center">
-                    <DataState state={data.qualify} />
-                  </div>
-                </div>
-              
-              <div className="flex gap-2 lg:gap-0 acc-body">
-                <p className="text-darkBlue lg:hidden">Area:</p>
-                <div className="flex justify-self-center">
-                  <p className="justify-self-center">{`${data.area}`}</p>
-                </div>
+      {dataStudents ? (
+        dataStudents.map((data, index)=>(
+          <div
+          className={`acc-item grid grid-cols-1 lg:grid-cols-[150px_minmax(350px,1fr)_minmax(250px,_1fr)_repeat(2,_minmax(100px,_1fr))] items-center gap-x-3 text-paragraph2 font-cocogooseLight text-black p-5 border-b-2 border-b-placeholderBlue ${
+            openAcc === index ? "open" : "close"
+          }`}
+          key={index}
+        >
+            <div className="flex gap-2 lg:gap-0">
+              <p className="text-darkBlue lg:hidden">No°:</p>
+              <div className="acc-header w-full flex justify-between items-center">
+                <p>
+                  {(index + 1).toString().length == 2
+                    ? index + 1
+                    : `0${index + 1}`}
+                </p>
+                <button onClick={() => toogleRow(index)}><i className="fa-solid fa-angle-down block lg:hidden"></i></button>
               </div>
             </div>
-          ))}
+            <div className="flex gap-2 lg:gap-0 ">
+              <p className="text-darkBlue lg:hidden">Nombre:</p>
+              <div className=" flex justify-self-center acc-header">
+                <p className="underline cursor-pointer"  onClick={() => getId(data.idestudiante)}>{`${data.nombre} ${data.apellido}`}</p>
+              </div>
+            </div>
+
+            <div className="flex gap-2 lg:gap-0 acc-body ">
+              <p className="text-darkBlue lg:hidden">Diagnostico:</p>
+              <div className="flex justify-self-center">
+                <p>{data.diagnostico}</p>
+              </div>
+            </div>
+
+            <div className="acc-body flex gap-2 lg:gap-0">
+                <p className="text-darkBlue lg:hidden">Calificación:</p>
+                <div className="w-full flex justify-between items-center">
+                  <DataState state={data.calificado} />
+                </div>
+              </div>
+            
+            <div className="flex gap-2 lg:gap-0 acc-body">
+              <p className="text-darkBlue lg:hidden">Area:</p>
+              <div className="flex justify-self-center">
+                <p className="justify-self-center">{`${data.area}`}</p>
+              </div>
+            </div>
+          </div>
+        ))
+      ):(
+        <p>¡No hay estudiantes registrados!</p>
+      )}
         </section>
-      </main>
+        </main>
     </>
   );
 }
