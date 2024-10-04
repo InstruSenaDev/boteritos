f015f87a6a49709471e2f22d85f6c12a6798df22
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-10-03 23:27:07 -0500

    validación medicamentos corregida

--- frontEnd/src/sections/admin/estudiantes/Detail.jsx
+++ frontEnd/src/sections/admin/estudiantes/Detail.jsx
@@ -116,7 +116,7 @@ const Detail = () => {
 
     switch (sectionId) {
       case "Datos personales":
-        data = dataDetail.personal[index];
+        data = dataDetail.usuario[index];
         break;
       case "Responsables":
         data = dataDetail.responsables[index];
@@ -268,7 +268,7 @@ const Detail = () => {
           dataDetail.usuario.map((value, index) => (
             <GrupoDatos
               titulo={"Datos personales"}
-              update={() => update("Datos Personales", index)}
+              update={() => update("Datos personales", index)}
               data={dataDetail.usuario}
               key={index}
             >
