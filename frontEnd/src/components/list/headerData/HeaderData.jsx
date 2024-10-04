62b708c69f157c9fc4fc4d95878bf6e163d12eed
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-10-04 08:14:17 -0500

    header data

--- frontEnd/src/components/list/headerData/HeaderData.jsx
+++ frontEnd/src/components/list/headerData/HeaderData.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getOneUser } from "../../../api/get";
 import { useNavigate } from "react-router-dom";
 import CardsData from "./CardsData";
-import ActionData from "./ActionData";
+import ActionData from "./actionData";
 
 const logros = [
   {
@@ -20,10 +20,12 @@ const logros = [
 ];
 
 //ESTUDIANTES
-export const HeaderData = ({ id, urlApi, urlGo ,typeLink, typeHeaderdata }) => {
+export const HeaderData = ({ id, urlApi, urlGo, typeLink, typeHeaderdata }) => {
+  const idTrimestre = JSON.parse(localStorage.getItem("trimestre"));
 
   const [dataCard1, setDataCard1] = useState({});
-  const [dataCard2, setDataCard2] = useState([]);
+  const [dataCard2, setDataCard2] = useState({});
+  const [dataCard3, setDataCard3] = useState([]);
 
   const navigate = useNavigate();
 
@@ -31,31 +33,40 @@ export const HeaderData = ({ id, urlApi, urlGo ,typeLink, typeHeaderdata }) => {
   useEffect(() => {
     const getData = async () => {
       if (id) {
-        const response = await getOneUser(`${urlApi}${id}`);
-        
-        setDataCard1(response.data.data.dataEstudiante);
-        setDataCard2(response.data.data.dataResponsable || []);
+        const response = await getOneUser(`${urlApi}${id}/${idTrimestre}`);
+        console.log(response);
+
+        setDataCard1(response.data.data.card1);
+        setDataCard2(response.data.data.card2);
+        setDataCard3(response.data.data.card3);
       }
     };
     getData();
   }, [id]);
-  
-  const linkTo = (id)=>{
 
-    if (typeLink == 'back') {
-      navigate(-1)
-      return
+  const linkTo = (id) => {
+    if (typeLink == "back") {
+      navigate(-1);
+      return;
     }
 
     if (id) {
-      navigate(`${urlGo}/${id}`)
+      navigate(`${urlGo}/${id}`);
     }
-        
-  }
+  };
 
   return (
-    <CardsData dataGraphic={logros} dataCard1={dataCard1} dataCard2={dataCard2} >
-      <ActionData type={typeLink} data={dataCard1.id} goTo={linkTo} typeHeaderdata={typeHeaderdata}/>
+    <CardsData
+      dataGraphic={dataCard3}
+      dataCard1={dataCard1}
+      dataCard2={dataCard2}
+    >
+      <ActionData
+        type={typeLink}
+        data={dataCard1.id}
+        goTo={linkTo}
+        typeHeaderdata={typeHeaderdata}
+      />
     </CardsData>
   );
 };
